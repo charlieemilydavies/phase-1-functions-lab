ee5ad4b368ee77c6948c29a970c1cc8289e37848
Charlie Davies <charlieemilydavies@gmail.com>
2024-06-11 20:11:03 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+  const hqLocation = 42;
+  return Math.abs(pickupLocation - hqLocation);
+}
+function distanceFromHqInFeet(pickupLocation) {
+  const blocks = distanceFromHqInBlocks(pickupLocation);
+  return blocks * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+  const blocksTravelled = Math.abs(destination - start);
+  return blocksTravelled * 264;
+}
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+    return 0; // Free sample
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02; // 2 cents per foot after the first 400 feet
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25; // Flat rate of 25 dollars
+  } else {
+    return "cannot travel that far"; // Distance over 2500 feet
+  }
+}
